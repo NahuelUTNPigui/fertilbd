7aadd7f39e5c96d9221617ebe7a56cee722f8ef2
nahuel facu <nahuel.utn.frc.piguillem@gmail.com>
2024-12-16 11:38:44 -0300

    cambio de nombre

---
+++ pb_migrations/1734312125_updated_animales.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bapofqo9",
+    "name": "lote",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "ws7ehbstl4qyf38",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bapofqo9",
+    "name": "lotes",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "ws7ehbstl4qyf38",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
