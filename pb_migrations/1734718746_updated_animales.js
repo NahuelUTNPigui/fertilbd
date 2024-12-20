746ba3045181fbd3edce6f9fbeeadc688f32cef1
nahuel facu <nahuel.utn.frc.piguillem@gmail.com>
2024-12-20 15:38:32 -0300

    cambio de preñada

---
+++ pb_migrations/1734718746_updated_animales.js
@@ -0,0 +1,46 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // remove
+  collection.schema.removeField("sjsli91n")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "o9rgq2sp",
+    "name": "prenada",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "sjsli91n",
+    "name": "prenada",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // remove
+  collection.schema.removeField("o9rgq2sp")
+
+  return dao.saveCollection(collection)
+})
