bb69c0de98d1c48ab29a1502fea9d56577907a29
nahuel facu <nahuel.utn.frc.piguillem@gmail.com>
2025-02-10 00:52:02 -0300

    animales motivo de baja2

---
+++ pb_migrations/1739136616_updated_animales.js
@@ -0,0 +1,31 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "r1zwwohk",
+    "name": "motivobaja",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("y6xc6bt2otpz2pb")
+
+  // remove
+  collection.schema.removeField("r1zwwohk")
+
+  return dao.saveCollection(collection)
+})
