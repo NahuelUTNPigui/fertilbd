45308e1dbaebe09e0dcc24f057f38beed8a61df7
nahuel facu <nahuel.utn.frc.piguillem@gmail.com>
2025-02-23 16:12:37 -0300

    ooks

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,3 +1,15 @@
+function showBody(e){
+    const info = e.requestInfo()
+    
+    Object.keys(info.body).forEach(function(key,index) {
+        console.log("KEY:" + key)
+        console.log("VALOR: " + info.body[key])
+    })
+}
+function getBody(e){
+    const info = e.requestInfo()
+    return info.body
+}
 //http://127.0.0.1:9991/
 routerAdd("GET", "/hello/{name}", (e) => {
     console.log("x")
@@ -5,5 +17,10 @@ routerAdd("GET", "/hello/{name}", (e) => {
     
     return e.json(200, { "message": "Hello " + name })
 })
+
+onRecordCreateRequest((e)=>{
+    showBody(e)
+    e.next()
+},'users')
 
 
